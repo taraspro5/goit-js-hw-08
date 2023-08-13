4af4caaa8ebd1f485c11d98c08d8cae4023a7ce2
taraspro5 <kovtun_taras007@ukr.net>
2023-08-13 13:31:26 +0300

    complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(handlerUpdate, 1000));
+
+function handlerUpdate(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
