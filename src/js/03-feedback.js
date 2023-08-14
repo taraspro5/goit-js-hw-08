ef2312af2f43b979679aa1f1de353fc695391fd9
taraspro5 <kovtun_taras007@ukr.net>
2023-08-14 20:33:26 +0300

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,44 @@
 import throttle from 'lodash.throttle';
 
-const selectors = {
+const elements = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('input'),
   textarea: document.querySelector('textarea'),
 };
+
 const KEY = 'feedback-form-state';
-const formStory = {};
+const { email, message } = elements.form;
+let formData = JSON.parse(localStorage.getItem(KEY)) ?? {};
+
+elements.form.addEventListener('input', throttle(handlerInput, 500));
+elements.form.addEventListener('submit', handlerSubmit);
+
 set();
 
-console.dir(selectors.form);
-selectors.form.addEventListener('input', throttle(handlerInput, 500));
-selectors.form.addEventListener('submit', handlerSubmit);
-
 function handlerInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+function handlerSubmit(evt) {
   evt.preventDefault();
-  formStory[evt.target.name] = evt.target.value;
-  localStorage.setItem(KEY, JSON.stringify(formStory));
+  if (email.value === '' && message.value === '') {
+    return alert('Fill all fields');
+  }
+  console.log(formData);
+  localStorage.removeItem(KEY);
+  elements.form.reset();
+  formData = {};
 }
 
 function set(evt) {
   const message = JSON.parse(localStorage.getItem(KEY));
   if (message) {
     if (message.email) {
-      selectors.input.value = message.email;
+      elements.input.value = message.email;
     }
     if (message.message) {
-      selectors.textarea.value = message.message;
+      elements.textarea.value = message.message;
     }
   }
-}
-
-function handlerSubmit(evt) {
-  evt.preventDefault();
-  localStorage.removeItem(KEY);
-  selectors.form.reset();
-  console.log(formStory);
 }
